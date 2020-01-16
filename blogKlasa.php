20f7e9d10e6dbaba2cf97c113f50f310fed9b504
ruzmi141 <58731227+ruzmi141@users.noreply.github.com>
2020-01-16 10:37:48 +0100

    Add files via upload

---
+++ blogKlasa.php
@@ -0,0 +1,90 @@
+<?php
+
+class Blog {
+
+	private $db;
+
+	public function __construct($db) {
+		$this->db = $db;
+	}
+
+	public function noviBlog() {
+		if(!isset($_POST['tip']) || !isset($_POST['naslov']) || !isset($_POST['opis']) || !isset($_POST['datum'])){
+			return false;
+
+		}
+		if($_POST['tip']=='' || $_POST['naslov']=='' || $_POST['opis'] == '' || $_POST['datum'] == ''){
+			return false;
+
+		}
+		$parameters = '[{'.
+
+			'"tipID"'.':'.'"'.$_POST['tip'].'",'.
+			'"naslov"'.':'.'"'.$_POST['naslov'].'",'.
+			'"opis"'.':'.'"'.$_POST['opis'].'",'.
+			'"datum"'.':'.'"'.$_POST['datum'].'"'
+
+			.'}]';
+
+
+			$curl_zahtev = curl_init("http://localhost/SeminarskiPsiholog/rest/noviBlog.json");
+			curl_setopt($curl_zahtev, CURLOPT_POST, TRUE);
+			curl_setopt($curl_zahtev, CURLOPT_POSTFIELDS, $parameters);
+			curl_setopt($curl_zahtev, CURLOPT_RETURNTRANSFER, 1);
+			$curl_odgovor = curl_exec($curl_zahtev);
+			$json_objekat=json_decode($curl_odgovor, true);
+			curl_close($curl_zahtev);
+
+
+
+			if($json_objekat == "Uspesno ste ubacili blog!") {
+				return true;
+			}
+			else {
+				return false;
+			}
+	}
+
+
+
+	public function izmeni($id) {
+		if(!isset($_POST['naslov']) || !isset($_POST['tip']) || !isset($_POST['opis']) || !isset($_POST['datum'])){
+			return false;
+
+		}
+		if($_POST['naslov']=='' || $_POST['tip']=='' || $_POST['opis'] == '' || $_POST['datum'] == ''){
+			return false;
+
+		}
+
+		$parameters = '[{'.
+
+			'"naslov"'.':'.'"'.$_POST['naslov'].'",'.
+			'"tipID"'.':'.'"'.$_POST['tip'].'",'.
+			'"opis"'.':'.'"'.$_POST['opis'].'",'.
+			'"prognoza"'.':'.'"'.$id.'",'.
+			'"datum"'.':'.'"'.$_POST['datum'].'"'
+
+			.'}]';
+
+
+			$curl_zahtev = curl_init("http://localhost/SeminarskiPsiholog/rest/izmenaBloga.json");
+			curl_setopt($curl_zahtev, CURLOPT_POST, TRUE);
+			curl_setopt($curl_zahtev, CURLOPT_POSTFIELDS, $parameters);
+			curl_setopt($curl_zahtev, CURLOPT_RETURNTRANSFER, 1);
+			$curl_odgovor = curl_exec($curl_zahtev);
+			$json_objekat=json_decode($curl_odgovor, true);
+			curl_close($curl_zahtev);
+
+
+			if($json_objekat == "Uspesno ste izmenili blog!") {
+				return true;
+			}
+			else {
+				return false;
+			}
+	}
+
+}
+
+?>
---
+++ footer.php
@@ -0,0 +1,20 @@
+<footer>
+        <div class="container">
+            <div class="row">
+
+                <div class="col-md-12">
+                    <ul class="list-inline social-buttons">
+                        <li><a href="https://twitter.com/igic98" target="_blank"><i class="fa fa-twitter"></i></a>
+                        </li>
+                        <li><a href="https://www.facebook.com/mihailo.igic1" target="_blank"><i class="fa fa-facebook"></i></a>
+                        </li>
+                        <li><a href="https://www.instagram.com/igic_28/" target="_blank"><i class="fa fa-instagram"></i></a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-12">
+                    <span class="copyright">Copyright &copy; Online Psiholog 2020</span>
+                </div>
+            </div>
+        </div>
+    </footer>
